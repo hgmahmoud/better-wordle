97e5b74dc3d7fff1b7728408c079dfc0c1b96b5c
Haithem <hgmahmoud97@gmail.com>
2022-02-22 22:04:58 -0600

    Spring Initializr

---
+++ better-wordle/src/main/java/com/better/wordle/betterwordle/BetterWordleApplication.java
@@ -0,0 +1,13 @@
+package com.better.wordle.betterwordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterWordleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterWordleApplication.class, args);
+	}
+
+}
